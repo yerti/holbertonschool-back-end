b6b0fafb1d8d80ee76c19bb9baa484bf196c7c2f
yerti <yerti0818@gamil.com>
2023-09-22 07:18:30 -0700

    Fix: remove comments and fix pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,8 +3,6 @@
 script that returns the user and a list
 of total tasks and completed tasks
 """
-
-
 import requests
 import sys
 
@@ -13,22 +11,23 @@ API = 'https://jsonplaceholder.typicode.com'
 
 
 endpoints = {
-        "users": f"{API}/users", # https://jsonplaceholder.typicode.com/users
-        "todos": f"{API}/todos" # https://jsonplaceholder.typicode.com/todos
+        "users": f"{API}/users",
+        "todos": f"{API}/todos"
         }
+
 
 def get_todo_list_progress(employee_id: int) -> str:
     """
     We get the employee with id employee_id
     """
-    get_employee_endpoint = f"{endpoints['users']}/{employee_id}" # https://jsonplaceholder.typicode.com/users/1
+    get_employee_endpoint = f"{endpoints['users']}/{employee_id}"
     employee_response = requests.get(get_employee_endpoint)
     employee = employee_response.json()
 
     """
     We get the list of tasks of the employee with id employee_id
     """
-    get_employee_todos_endpoint = f"{endpoints['todos']}?userId={employee_id}" # https://jsonplaceholder.typicode.com/todos?userId=1
+    get_employee_todos_endpoint = f"{endpoints['todos']}?userId={employee_id}"
     todos_response = requests.get(get_employee_todos_endpoint)
     todos = todos_response.json()
 
@@ -40,17 +39,19 @@ def get_todo_list_progress(employee_id: int) -> str:
     completed_todos = []
 
     for todo in todos:
-        if todo["completed"] == True:
+        if todo["completed"] is True:
             completed_todos.append(todo)
-    completed_todos_quantity = len(completed_todos)
+    com_to_qu = len(completed_todos)
 
-    first_line = f"Employee {employee['name']} is done with tasks({completed_todos_quantity}/{todos_quantity}):\n"
+    employee = "Employee {employee['name']} is done with tasks"
+    first_line = f"{employee}({com_to_qu}/{todos_quantity}):\n"
 
     next_line = ''
     for completed_todo in completed_todos:
         next_line = next_line + "\t " + completed_todo["title"] + "\n"
 
     return first_line + next_line
+
 
 employee_id = sys.argv[1]
 
